e27358d3a280da778ea70eb29dd0c447780607ac
TamilselvanChandrakumar <tamilbin001@gmail.com>
2024-01-29 18:36:22 +0530

    first commit

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ let todolist = document.getElementById("todolist");
 let input = document.getElementById("input");
 
 let todos = [];
+window.onload = () => {
+  todos = JSON.parse(localStorage.getItem("todos")) || [];
+  todos.forEach((todo) => addTodo(todo));
+};
 add.addEventListener("click", () => {
   todos.push(input.value);
-  console.log(todos);
+  localStorage.setItem("todos", JSON.stringify(todos));
   addTodo(input.value);
   input.value = "";
 });
@@ -14,6 +18,7 @@ function addTodo(todo) {
   let para = document.createElement("p");
   para.innerText = todo;
   todolist.appendChild(para);
+
   para.addEventListener("click", () => {
     para.style.textDecoration = "line-through";
     remove(todo);
@@ -27,7 +32,8 @@ function addTodo(todo) {
 }
 function remove(todo) {
   let index = todos.indexOf(todo);
-  //   if (index > -1) {
-  todos.splice(index, 1);
-  //   }
+  if (index > -1) {
+    todos.splice(index, 1);
+  }
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
